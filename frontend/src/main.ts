146ff13a70804fb4c04a3d4227ca2120f752e56f
Sam Webster <sam.f.webster1@gmail.com>
2025-04-13 15:35:55 +1000

    update svelte 4 to svelte 5

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,9 @@
 import "./custom-preflight.css";
 import "./style.css";
 import App from "./App.svelte";
+import { mount } from "svelte";
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById("app") as Document | Element | ShadowRoot,
 });
 
